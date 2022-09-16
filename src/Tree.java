3e66e4be65017042f74d534a3f000bf04271b85f
Anika Iyer <anika.sule.iyer@gmail.com>
2022-09-16 02:27:16 -0700

    Finished Tree
    Exactly what it sounds like. Still need to test. Will be making a tester now. I need sleep.

--- src/Tree.java
+++ src/Tree.java
@@ -15,19 +15,9 @@ import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class Tree {
-	HashMap<String, String> map = new HashMap<String, String>();
 	
 	public Tree(ArrayList<String> blobList) throws NoSuchAlgorithmException, UnsupportedEncodingException
 	{
-		String key, value;
-		
-		// Put values into a hashMap
-		for(String str : blobList)
-		{
-			key = str.substring(0, str.indexOf(":"));
-			value = str.substring(str.indexOf(":") + 2, str.length());
-			map.put(key, value);
-		}
 		
 		// Create a giant string of all the strings in the ArrayList
 		String allStrings = "";
@@ -38,9 +28,13 @@ public class Tree {
 		
 		String sha1 = Blob.getSHA1(allStrings);
 		
-		
-	}
-	
-	
-
+		// This code is from my own Blob method. Convenient how that works out. Basically creates the file with the sha1 as the name.
+		Path p = Paths.get(".\\objects\\" + sha1);
+        try {
+            Files.writeString(p, allStrings, StandardCharsets.ISO_8859_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }		
+        
+	}	
 }
